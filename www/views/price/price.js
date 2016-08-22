0ce094c2f5bfeb16a9df21946aa17eacee60c85c
danding <1291248710@qq.com>
2016-08-22 15:55:34 +0800

    xx

--- www/views/price/price.js
+++ www/views/price/price.js
@@ -2,9 +2,25 @@
  * Created by danding on 16/7/27.
  */
 angular.module('app')
-  .controller('priceController', function ($scope,$http,$state) {
+  .controller('priceController', function ($scope,$http,$state,$ionicModal) {
 
     $scope.prices=[];
+
+    //initial modal dialog
+    $ionicModal.fromTemplateUrl('edit.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.modalOpen=function(){
+      $scope.modal.show();
+    }
+
+    $scope.modalClose=function(){
+      $scope.modal.hide();
+    }
 
     $http({
       method:"get",
@@ -27,8 +43,19 @@ angular.module('app')
     }
 
     $scope.proj_select=function(price){
+      $scope.modal.show();
       console.log('price name='+price.name);
       $scope.price={name:price.name,fee:price.fee,detail:price.detail};
     };
+
+    $scope.generate_order=function(){
+      console.log('...');
+      $scope.modal.hide();
+    }
+
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+
 
   });
