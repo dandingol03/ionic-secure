e6b757a4db4a4ad0a15d339e440719da6889e860
danding <1291248710@qq.com>
2016-08-22 09:56:06 +0800

    xx

--- www/views/coverage/coverage.js
+++ www/views/coverage/coverage.js
@@ -3,6 +3,10 @@ angular.module('app')
 
 
     $scope.coverages=[];
+
+    $scope.projects={};
+    $scope.projects.selected=[];
+
     $http({
       method:"get",
       url:"/proxy/node/insurance/projecct_provide"
@@ -10,11 +14,20 @@ angular.module('app')
         var projects=response.projects;
         if(Object.prototype.toString.call(projects)!='[object Array]')
           projects=JSON.parse(projects);
+          projects.map(function(project,i) {
+              project.selected=project.fee[0];
+          });
         $scope.coverages=projects;
 
     }).error(function(err){
       console.error(err.toString());
     });
+
+    $scope.coverage_change=function(item){
+      var coverages=$scope.coverages;
+      console.log('...');
+      console.log('...');
+    }
 
 
   });
