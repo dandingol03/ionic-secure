251f379e56c8fc476b40e925f27a8899f8c568ee
apple-1 <651090371@qq.com>
2016-08-24 15:24:46 +0800

    1

--- www/views/car_info/car_info.js
+++ www/views/car_info/car_info.js
@@ -5,34 +5,30 @@
 
 
 angular.module('app')
-  .controller('carInfoController',function($scope,$http,$state,ionicDatePicker,$cordovaImagePicker,$ionicActionSheet,$ionicPopover){
+  .controller('carInfoController',function($scope,$http,$state,ionicDatePicker,$cordovaImagePicker,$ionicActionSheet,$ionicLoading){
         $scope.car=new Object();
         $scope.nextStep=function(){
 
             $http({
                 method:"post",
                 params:{
-                    perName:$scope.perName,
-                    perIdCard:$scope.perIdCard,
+                  carInfo:
+                  {
+                    perName:$scope.car.perName,
+                    perIdCard:$scope.car.perIdCard,
                     plateNum:$scope.car.plateNum,
                     Model:$scope.car.model,
                     VIN:$scope.car.VIN,
                     engineNum:$scope.car.engineNum,
                     registerDate:$scope.car.registerDate,
                     inssueDate:$scope.car.inssueDate
+                  }
                 },
-                url:"http://localhost:9030/get_render_page.do"
+                url:"/proxy/node/insurance/car_info_upload.do"
             }).success(function(response){
-                var errorMsg =  response.errorMsg;
-                if(errorMsg !== null && errorMsg !== undefined && errorMsg !== ""){
-                    alert(errorMsg);
-                }else{
-                    var re = response.re;
-                    var arr=response.arr;
-                    $state.go("life_insurance",{arr:arr});
-                }
+              var re = response.re;
+
             }).error(function(err){
-                alert(err.toSource());
                 $ionicLoading.show({
                     template:'connect the server timeout',
                     duration:'2000'
