427f49618ea1f0f5c0bc972d1f9b2245c827e503
EEvvee <dingyiming_sdu@163.com>
2016-08-24 10:03:21 +0800

    dym

--- www/views/personInformation/personInformation.js
+++ www/views/personInformation/personInformation.js
@@ -2,5 +2,35 @@
  * Created by apple-2 on 16/8/23.
  */
 angular.module('app')
-  .controller('personInformationController',function($scope,$state,$http) {
+  .controller('personInformationController',function($scope,$state,$http,$ionicModal) {
+
+    $scope.newTel=''
+    $scope.oldPwd=''
+    $scope.conflict=false;
+    $scope.pwd={};
+
+    $ionicModal.fromTemplateUrl('edit.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+
+
+    $scope.change=function () {
+      if($scope.pwd.newPwd.length==$scope.pwd.confirm_pwd.length)
+      {
+        if($scope.pwd.newPwd!=$scope.pwd.confirm_pwd)
+        {
+            $scope.conflict=true;
+        }
+      }
+      else{
+        $scope.conflict=false;
+      }
+      $scope.conflict=false;
+    }
+
+
   })
