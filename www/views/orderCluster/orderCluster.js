f46b401eeb6bb3e47c761dd796e98749943ff379
danding <1291248710@qq.com>
2016-09-03 13:04:05 +0800

    xx

--- www/views/orderCluster/orderCluster.js
+++ www/views/orderCluster/orderCluster.js
@@ -4,16 +4,33 @@ angular.module('app')
     //TODO:add selecte item css highlight
 
     $scope.tabs=[
-      {name:'估价中',activated:true},
-      {name:'历史订单',activated:false},
-      {name:'已生成订单',activated:false},
-      {name:'购买意向',activated:false}
+      {name:'待支付订单',activated:false,orders:
+        [
+        {
+          type:'车险',schemes:[
+          {name:'brazil',coverage:300,fee:500,spectum:2},
+          {name:'honda',coverage:300,fee:500,spectum:1},
+          {name:'R1',coverage:300,fee:500,spectum:1},]
+        },
+        {
+          type:'寿险',main:{name:'india洋',coverage:300,fee:200},additions:[
+          {name:'addition1',fee:300},
+          {name:'addition2',fee:800}]
+        },
+        {
+          type:'寿险',main:{name:'india洋',coverage:300,fee:200},additions:[
+          {name:'addition1',fee:300},
+          {name:'addition2',fee:800}]
+        }
+      ]},
+      {name:'已完成订单',activated:false,orders:[]},
+      {name:'估价中订单',activated:false,orders:[]}
     ];
 
+    $scope.tabIndex=0;
+
     $scope.tab_change=function(i){
-      console.log('...');
-      console.log('...');
-      console.log('...');
+      $scope.tabIndex=i;
     }
 
     $scope.go_back=function(){
